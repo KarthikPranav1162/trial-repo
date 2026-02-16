51d920a0b39d26960c5ae4a88fcbd6e1cc823973
KarthikPranav1162 <karthikpranav2002@gmail.com>
2026-02-16 11:09:06 +0530

    new commit

---
+++ sub.java/src/sub/java/subtract.java
@@ -0,0 +1,12 @@
+package sub.java;
+
+public class subtract {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 15, b = 10;
+int del = a - b;
+System.out.println("calc = " +del);
+	}
+
+}
